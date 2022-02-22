fc3b5f0d014e508cb6c9815f8bce85ada1d0f52d
thant <90088844+thantzan212@users.noreply.github.com>
2022-02-22 12:11:15 +0630

    Update google.py

--- Zaid/modules/google.py
+++ Zaid/modules/google.py
@@ -287,7 +287,7 @@ __help__ = """
 ❂ /github <username>*:* Get information about a GitHub user.
 ❂ /country <country name>*:* Gathering info about given country
 ❂ /imdb <Movie name>*:* Get full info about a movie with imdb.com
-❂ Emiko <query>*:* Emiko answers the query
+❂ botညီလေး <query>*:* botညီလေး answers the query
 
-  💡Ex: `Emiko where is Japan?`
+  💡Ex: `Botညီလေး where is Japan?`
 """
