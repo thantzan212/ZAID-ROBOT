f0a2b9b558e688b4b00393b09f483a4c5393e096
thant <90088844+thantzan212@users.noreply.github.com>
2022-02-22 10:24:19 +0630

    botnyilay

--- Zaid/modules/alive.py
+++ Zaid/modules/alive.py
@@ -9,16 +9,16 @@ from Zaid.events import register
 from Zaid import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/be24bbabbe0ec30dff489.jpg"
+PHOTO = "https://telegra.ph/file/8c278d46b852306e18231.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Zaid Robot.** \n\n"
   TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Master : [Zaid](https://t.me/Timesisnotwaiting)** \n\n"
+  TEXT += f"⚪ **My Master : [Aₙₜᵢₜₕₑₛᵢₛ](https://t.me/olo_pl_ya_mal)** \n\n"
   TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/Zaid2_Robot?start=help"), Button.url("Support", "https://t.me/Superior_Support")]]
+  BUTTON = [[Button.url("Help", "https://t.me/n4nd4_hippy_bot?start=help"), Button.url("Support", "https://t.me/+-Gsapl_ciLYwMGNl")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
