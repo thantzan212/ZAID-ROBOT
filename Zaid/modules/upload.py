c50291b223130cd2ed3dd1089e8c9e2bc4d737e5
thant <90088844+thantzan212@users.noreply.github.com>
2022-02-22 12:18:25 +0630

    Update upload.py

--- Zaid/modules/upload.py
+++ Zaid/modules/upload.py
@@ -83,7 +83,7 @@ async def tsh(event):
 
             str(time.time() - start)
             await orta.edit(
-                f"File Successfully Uploaded to TransferSh.\n\nLink 👉 {download_link}\nExpired Date 👉 {final_date}\n\nUploaded by *AsunaRobot*"
+                f"File Successfully Uploaded to TransferSh.\n\nLink 👉 {download_link}\nExpired Date 👉 {final_date}\n\nUploaded by *Botညီလေး*"
             )
         except Exception as e:
             traceback.print_exc()
@@ -159,7 +159,7 @@ async def up(event):
                 event.chat.id,
                 dosya,
                 force_document=True,
-                caption=f"Uploaded By *AsunaRobot*",
+                caption=f"Uploaded By *Botညီလေး*",
             )
         except Exception as e:
             traceback.print_exc()
